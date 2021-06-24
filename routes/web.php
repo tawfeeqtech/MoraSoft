0f7c65579d29d56f33001df25cf4adc93d4b67ef
tawfeeq <tawfeeq.tech@gmail.com>
2021-06-25 02:14:05 +0300

    Mcamara Localization translate

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('dashboard');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ], function () { //...
+    Route::get('/', function () {
+        return view('dashboard');
+    });
 });
+
+
